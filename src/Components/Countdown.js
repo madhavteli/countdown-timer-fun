84943093aca990a0221906411586f7b4c2fa1e84
mteli <mteli@xento.com>
2021-09-14 10:53:48 +0530

    changes final

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 const Countdown = ( props ) => {
-	let [count, setCount] = useState( props.number ? props.number : 30 );
+	let   [count, setCount] = useState( props.number ? props.number : 30 );
 
 	useEffect(()=> {
 		const interval = setInterval( () => {
 			if(count > 0 ){
+
+
+
 				setCount(--count);
 			}
 		}, 1000 );
@@ -16,6 +19,7 @@ const Countdown = ( props ) => {
 
 	return (
 		<div style={{ textAlign: 'center' }}>
+
 			<h1>{count}</h1>
 			{count === 0 && <p>Time's up!</p>}
 		</div>
